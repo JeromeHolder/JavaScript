43d5c791fe4888ec414e6baf6205c22ca69231f0
JeromeHolder <j.e.holder@outlook.com>
2018-02-22 15:14:24 -0500

    6th commit

---
+++ logical.js
@@ -0,0 +1,18 @@
+console.log(5<3)
+
+//using &&
+console.log(5>3) && (2<3)
+
+//&& with a variable and using string.length
+var string = "hello";
+console.log(string.length > 10 && (9 * 2 == 18))
+
+// using ||
+console.log(5<3) || (2>3)
+
+//using || with a variable and using string.length
+var string = "hello"
+console.log(string.length > 10 || (9 * 2 == 18))
+
+//using ! NOT
+console.log(!(1 == 2))
