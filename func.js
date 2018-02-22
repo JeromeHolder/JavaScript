6da06dc49f317c7d101749aa1004000c5faaf4b0
JeromeHolder <j.e.holder@outlook.com>
2018-02-22 15:58:57 -0500

    7th commit

---
+++ func.js
@@ -0,0 +1,10 @@
+//basic variable expression
+var answer = 2 + 2;
+console.log(answer);
+
+//function
+function myFunction(){
+	var answer1 = 1 + 2;
+	// unsure where/how to use this >> console.log(answer1)
+}
+
